dca117fb2416f073034cd85d70fea3615c0a9a68
kalsing <gabriel.kalsing@gmail.com>
2026-01-16 13:13:33 -0300

    feat: added SQLite database class

---
+++ database.js
@@ -0,0 +1,18 @@
+const sqlite3 = require('sqlite3').verbose()
+const dbName = 'UsersDB'
+
+let db = new sqlite3.Database(dbName, (err) => {
+    if(err){
+        console.log(err.message)
+    }
+    else{
+        console.log("Connceted") 
+        db.run('CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,', (err) => {})}
+        if(err){
+            console.error(err.message)
+        }else{
+            console.log("Table created")
+        }
+})
+
+module.exports = db;
