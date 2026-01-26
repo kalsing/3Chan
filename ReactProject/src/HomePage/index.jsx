a8b1cda67b381936768cc865b327d967bd135f68
kalsing <gabrilel.kalsing@gmail.com>
2026-01-26 15:34:57 -0300

    feat: created html skeleton of homepage

---
+++ ReactProject/src/HomePage/index.jsx
@@ -0,0 +1,15 @@
+function HomePage(){
+    return (
+
+
+
+        
+        <div className="tantofaz">
+                <input type="text" />
+                <input type="text" />
+                <button>Enviar</button>
+            </div>
+    );
+}
+
+export default HomePage;
