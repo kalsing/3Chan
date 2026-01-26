64cedc5884dd85a55bcb0a3bb5dc452fc552768a
kalsing <gabrilel.kalsing@gmail.com>
2026-01-26 16:02:42 -0300

    feat: frontend integration with backend now working with basic html

--- ReactProject/src/HomePage/index.jsx
+++ ReactProject/src/HomePage/index.jsx
@@ -1,15 +1,34 @@
-function HomePage(){
-    return (
+import { useState } from "react"
+import api from "../apis/api"
 
+function HomePage() {
+  const [nome, setNome] = useState("")
+  const [sobrenome, setSobrenome] = useState("")
 
+  async function postUser() {
+    await api.post("/users", {
+    firstName: nome,
+    lastName: sobrenome
+    })}
 
-        
-        <div className="tantofaz">
-                <input type="text" />
-                <input type="text" />
-                <button>Enviar</button>
-            </div>
-    );
-}
+  return (
+<div className="tantofaz">
+  <input
+        type="text"
+        placeholder="Nome"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+/>
 
-export default HomePage;
+ <input
+        type="text"
+        placeholder="Sobrenome"
+        value={sobrenome}
+        onChange={(e) => setSobrenome(e.target.value)}
+/>
+
+    <button onClick={postUser}>Enviar</button>
+    </div>
+  )}
+
+export default HomePage
