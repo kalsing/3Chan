a6f130130ca46a069554e0a0e4daff84887f5c95
kalsing <gabrilel.kalsing@gmail.com>
2026-01-30 09:58:10 -0300

    feat: new imports for icons and font from material UI

--- ReactProject/src/main.jsx
+++ ReactProject/src/main.jsx
@@ -1,9 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Index from "./Pages/HomePage"
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Index from "./Pages/HomePage/Homepage"
+import HomePage from './Pages/HomePage/Homepage';
+
+import { CssBaseline } from "@mui/material";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Index />
-  </StrictMode>,
-)
+    <>
+      <CssBaseline />
+      <HomePage />
+    </>
+  </StrictMode>
+);
