c149297a46fc2b74ce50b502e19d7bad5beac017
kalsing <gabrilel.kalsing@gmail.com>
2026-02-20 12:10:38 -0300

    feat: routes.js changed to have middlewares (not working properly yet)

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ import PostControl from "./controllers/PostControl.js";
 import UserControl from "./controllers/UserControl.js";
 import LikeControl from "./controllers/LikeControl.js";
 import LoginControl from "./controllers/LoginControl.js";
+import { Middleware } from "./controllers/Middleware.js";
 
 const router = Router();
 const postController = new PostControl();
@@ -10,20 +11,19 @@ const userController = new UserControl();
 const likeController = new LikeControl();
 const loginController = new LoginControl();
 
-router.post("/posts", (req, res) => postController.create(req, res));
-router.get("/posts", (req, res) => postController.showAll(req, res));
-router.put("/posts/:id", (req, res) => postController.update(req, res));
-router.delete("/posts/:id", (req, res) => postController.destroy(req, res));
-
+router.post("/posts", Middleware, (req, res) => postController.create(req, res));
+router.get("/posts", Middleware, (req, res) => postController.showAll(req, res));
+router.put("/posts/:id", Middleware, (req, res) => postController.update(req, res));
+router.delete("/posts/:id", Middleware, (req, res) => postController.destroy(req, res));
 
 router.post("/users", (req, res) => userController.createUser(req, res));
-router.get("/users", (req, res) => userController.showAllUsers(req, res));
-router.put("/users/:id", (req, res) => userController.updateUser(req, res));
-router.delete("/users/:id", (req, res) => userController.destroyUser(req, res));
+router.get("/users", Middleware, (req, res) => userController.showAllUsers(req, res));
+router.put("/users/:id", Middleware, (req, res) => userController.updateUser(req, res));
+router.delete("/users/:id", Middleware, (req, res) => userController.destroyUser(req, res));
 
-router.post("/likes", (req, res) => likeController.like(req, res));
-router.get("/posts/:postId/likes", (req, res) => likeController.likeCounter(req, res));
+router.post("/likes", Middleware, (req, res) => likeController.like(req, res));
+router.get("/posts/:postId/likes", Middleware, (req, res) => likeController.likeCounter(req, res));
 
-router.post("/login", (req, res) => loginController.loginUser(req, res));
+router.post("/login", (req, res) => loginController.loginUser(req, res)); 
 
 export default router;
